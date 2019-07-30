3afebe2e89f91fe15ca81d83a2df460fba6a94f3
kshitish pokharel <kshitishpokharel@gmail.com>
2019-07-30 15:02:06 +0545

    changes

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,21 +4,21 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     number = models.CharField(max_length=10)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    number = models.CharField(max_length=10)
 
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 
-# post_save.connect(create_user_profile, sender=User,
-#                   dispatch_uid="users_signal")
+post_save.connect(create_user_profile, sender=User,
+                  dispatch_uid="users_signal")
 
 
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
