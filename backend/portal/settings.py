b359eff86f181a6587c55c0468cb30adbcf201cf
Sugat Paneru <sugatp454@gmail.com>
2019-10-03 14:00:23 +0545

    changes

--- backend/portal/settings.py
+++ backend/portal/settings.py
@@ -83,7 +83,7 @@ if 'aa1hihfy0ni4ce9.cenqftt2gi2y.ap-south-1.rds.amazonaws.com' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['ebdb'],
+            'NAME': os.environ['portal'],
             'USER': os.environ['djgroup'],
             'PASSWORD': os.environ['djgroup1234'],
             'HOST': os.environ['aa1hihfy0ni4ce9.cenqftt2gi2y.ap-south-1.rds.amazonaws.com'],
