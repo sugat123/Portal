eeeec226f914252493c6ece354b1c344a871abd9
Sugat Paneru <sugatp454@gmail.com>
2019-10-03 13:55:07 +0545

    final

--- backend/portal/settings.py
+++ backend/portal/settings.py
@@ -79,15 +79,15 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-if 'RDS_DB_NAME' in os.environ:
+if 'aa1hihfy0ni4ce9.cenqftt2gi2y.ap-south-1.rds.amazonaws.com' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'NAME': os.environ['aa1hihfy0ni4ce9'],
+            'USER': os.environ['djgroup'],
+            'PASSWORD': os.environ['djgroup1234'],
+            'HOST': os.environ['aa1hihfy0ni4ce9.cenqftt2gi2y.ap-south-1.rds.amazonaws.com'],
+            'PORT': os.environ['3306'],
         }
     }
 else:
