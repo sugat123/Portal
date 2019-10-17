69996159d99b34b9119d99ec4c52fae656ecd964
Sugat Paneru <sugatp454@gmail.com>
2019-10-17 16:14:28 +0545

    changes

--- backend/portal/settings.py
+++ backend/portal/settings.py
@@ -198,4 +198,4 @@ AWS_S3_OBJECT_PARAMETERS = {
 # ]
 # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'portal.storage_backends.MediaStorage'
