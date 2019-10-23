e7509312de591234686bbc87fb06400ffe5397a6
Sugat Paneru <sugatp454@gmail.com>
2019-10-23 11:42:28 +0545

    cg

--- backend/portal/settings.py
+++ backend/portal/settings.py
@@ -201,3 +201,4 @@ AWS_LOCATION = 'media'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'portal.storage_backends.MediaStorage'
 MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+MEDIA_ROOT = MEDIA_URL
